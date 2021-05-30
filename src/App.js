f262f340b7421d5b7e41647c2045ec35e9b32a23
kazuki19992 <kushida98@gmail.com>
2021-05-30 16:27:22 +0900

    フッター(仮)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header'
+import Footer from './Components/Footer'
 
 import Top from './Views/Top'
 import { BrowserRouter as Router, Route} from 'react-router-dom'
@@ -13,6 +14,7 @@ function App() {
         <div className="contents-wrapper">
           <Route exact path='/' component={Top} />
         </div>
+        <Footer />
       </Router>
     </div>
   );
